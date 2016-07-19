f74940789e44dee9fe6427ffa3635736f711d685
radhika3049 <radhikasharma.3049@gmail.com>
2016-07-19 18:03:57 +0530

    write comment

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+Template.movies.helpers({
+         movieList:function() {
+             var array=[ 
+                     {movie:"x-men"},
+                    {movie:"conjuring 2"},
+                       {movie:"deadpool"},
+                       {movie:"minions"},
+];
+                      return array;
+  },
+inder:"preet"
+});
+
+
